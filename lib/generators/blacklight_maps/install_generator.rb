e588e2036fa82fbc9ff6a0de9ac341c9be3ce7ba
Jack Reed <phillipjreed@gmail.com>
2016-06-22 16:52:34 -0400

    add Leaflet-rails to the project

--- lib/generators/blacklight_maps/install_generator.rb
+++ lib/generators/blacklight_maps/install_generator.rb
@@ -5,5 +5,12 @@ module BlacklightMaps
     def add_gems
       gem 'blacklight_maps'
     end
+
+    source_root File.expand_path('../templates', __FILE__)
+
+    def assets
+      copy_file 'blacklight_maps.scss', 'app/assets/stylesheets/blacklight_maps.scss'
+      copy_file 'blacklight_maps.js', 'app/assets/javascripts/blacklight_maps.js'
+    end
   end
 end
