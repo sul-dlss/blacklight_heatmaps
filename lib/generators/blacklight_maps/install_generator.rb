abe3f35dbf7a65b1606d8b2c01a991facf024453
Jack Reed <phillipjreed@gmail.com>
2016-06-22 08:57:14 -0400

    adds Blacklight dependency and install generator

---
+++ lib/generators/blacklight_maps/install_generator.rb
@@ -0,0 +1,6 @@
+require 'rails/generators'
+
+module BlacklightMaps
+  class Install < Rails::Generators::Base
+  end
+end
--- spec/test_app_templates/lib/generators/test_app_generator.rb
+++ spec/test_app_templates/lib/generators/test_app_generator.rb
@@ -3,6 +3,19 @@ require 'rails/generators'
 class TestAppGenerator < Rails::Generators::Base
   source_root './spec/test_app_templates'
 
+  def add_gems
+    gem 'blacklight', '~> 6.0'
+    Bundler.with_clean_env do
+      run 'bundle install'
+    end
+  end
+
+  def run_blacklight_generator
+    say_status('warning', 'GENERATING BL', :yellow)
+
+    generate 'blacklight:install', '--devise'
+  end
+
   # if you need to generate any additional configuration
   # into the test app, this generator will be run immediately
   # after setting up the application
