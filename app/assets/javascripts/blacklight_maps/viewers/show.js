c607c9c072efeb519a9234dd6f1503e35ab907b7
Jack Reed <phillipjreed@gmail.com>
2016-06-24 15:06:33 -0400

    move show javascript under ./viewers

---
+++ app/assets/javascripts/blacklight_maps/viewers/show.js
@@ -0,0 +1,17 @@
+Blacklight.onLoad(function () {
+  'use strict';
+
+  $('[data-show-map]').each(function () {
+    var $el = $(this);
+    var features = $el.data().features;
+
+    var map = L.map($el[0].id).setView([0, 0], 1);
+    var basemap = L.tileLayer($el.data().basemap, {
+      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="http://cartodb.com/attributions">CartoDB</a>',
+    }).addTo(map);
+
+    var features = L.geoJson(features).addTo(map);
+
+    map.fitBounds(features.getBounds());
+  });
+});
