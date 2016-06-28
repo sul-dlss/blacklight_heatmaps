97a00dccda3d5bf8f906fb044133c954dbe2a2d5
Jack Reed <phillipjreed@gmail.com>
2016-06-28 13:38:05 -0700

    add helpers to application

--- lib/blacklight_maps/engine.rb
+++ lib/blacklight_maps/engine.rb
@@ -1,5 +1,8 @@
 module BlacklightMaps
   class Engine < ::Rails::Engine
     isolate_namespace BlacklightMaps
+    initializer 'blacklight-maps.helpers' do |app|
+      ActionView::Base.send :include, Blacklight::MapsHelper
+    end
   end
 end
