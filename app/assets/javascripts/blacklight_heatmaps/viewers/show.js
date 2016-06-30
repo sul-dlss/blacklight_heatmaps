e0553b35517f8eae53aec05a3057430abbab1a74
Jack Reed <phillipjreed@gmail.com>
2016-06-30 10:54:09 -0700

    Move project JavaScript to a more modular approach.

--- app/assets/javascripts/blacklight_heatmaps/viewers/show.js
+++ app/assets/javascripts/blacklight_heatmaps/viewers/show.js
@@ -2,16 +2,33 @@ Blacklight.onLoad(function () {
   'use strict';
 
   $('[data-show-map]').each(function () {
-    var $el = $(this);
-    var features = $el.data().features;
-
-    var map = L.map($el[0].id).setView([0, 0], 1);
-    var basemap = L.tileLayer($el.data().basemap, {
-      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="http://cartodb.com/attributions">CartoDB</a>',
-    }).addTo(map);
-
-    var features = L.geoJson(features).addTo(map);
-
-    map.fitBounds(features.getBounds());
+    BlacklightHeatmaps.showView(this);
   });
 });
+
+!(function (global) {
+  'use strict';
+
+  var ShowView = L.Class.extend({
+    options: {},
+
+    initialize: function (el, options) {
+      var $el = $(el);
+      var features = $el.data().features;
+
+      var map = L.map($el[0].id).setView([0, 0], 1);
+      var basemap = L.tileLayer($el.data().basemap, {
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="http://cartodb.com/attributions">CartoDB</a>',
+      }).addTo(map);
+
+      var features = L.geoJson(features).addTo(map);
+
+      map.fitBounds(features.getBounds());
+    },
+  });
+
+  global.BlacklightHeatmaps.ShowView = ShowView;
+  global.BlacklightHeatmaps.showView = function (el, options) {
+    return new ShowView(el, options);
+  };
+})(this);
