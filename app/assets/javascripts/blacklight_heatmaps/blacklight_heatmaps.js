c2d8b3b15e8e5bcee6b69a71af392a352846cced
Jack Reed <phillipjreed@gmail.com>
2016-07-11 16:27:11 -0400

    bump to 0.0.2

--- app/assets/javascripts/blacklight_heatmaps/blacklight_heatmaps.js
+++ app/assets/javascripts/blacklight_heatmaps/blacklight_heatmaps.js
@@ -3,7 +3,7 @@
 
   var BlacklightHeatmaps = L.Class.extend({
     statics: {
-      __version__: '0.1.0',
+      __version__: '0.0.2',
     },
   });
   global.BlacklightHeatmaps = BlacklightHeatmaps;
