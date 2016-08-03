5af231d853bba5088ce25d298050ad698e86e300
Jack Reed <phillipjreed@gmail.com>
2016-08-03 09:37:02 -0400

    Add support for multi-valued srpt indexing

--- app/models/concerns/blacklight_heatmaps/geometry_solr_document.rb
+++ app/models/concerns/blacklight_heatmaps/geometry_solr_document.rb
@@ -4,8 +4,18 @@ module BlacklightHeatmaps
   module GeometrySolrDocument
     def to_geojson(blacklight_config = nil)
       return unless blacklight_config.try(:geometry_field) && fetch(blacklight_config.geometry_field, nil)
-      BlacklightHeatmaps::GeometryParser
-        .parse(fetch(blacklight_config.geometry_field)).to_geojson
+      {
+        type: 'FeatureCollection',
+        features: Array(fetch(blacklight_config.geometry_field)).map do |geometry|
+          {
+            type: 'Feature',
+            geometry: JSON.parse(
+              BlacklightHeatmaps::GeometryParser.parse(geometry).to_geojson
+            ),
+            properties: {}
+          }
+        end
+      }.to_json
     end
   end
 end
--- spec/features/show_page_map_spec.rb
+++ spec/features/show_page_map_spec.rb
@@ -8,8 +8,11 @@ feature 'Show map map', js: true do
     expect(page).to have_css 'img[src*="light_all/4/11/5.png"]'
     expect(page).to have_css 'svg g path'
   end
-  it 'renders a point type' do
+  it 'renders a point type and a polygon' do
     visit solr_document_path '34860108'
-    expect(page).to have_css '.leaflet-marker-icon'
+    within '.leaflet-container' do
+      expect(page).to have_css 'svg g path'
+      expect(page).to have_css '.leaflet-marker-icon'
+    end
   end
 end
