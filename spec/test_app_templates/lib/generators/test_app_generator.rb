279ec1e5cac150c6ec4dcb67a457a0caad3d281b
Chris Beer <cabeer@stanford.edu>
2024-09-25 14:22:46 -0700

    Handle sprockets assets for Blacklight 8.

--- spec/test_app_templates/lib/generators/test_app_generator.rb
+++ spec/test_app_templates/lib/generators/test_app_generator.rb
@@ -24,4 +24,13 @@ class TestAppGenerator < Rails::Generators::Base
   def install_engine
     generate 'blacklight_heatmaps:install'
   end
+
+  # Temporarily force js assets to fall back to sprockets
+  def clean_up_js_builds
+    return unless File.exist?('app/assets/builds')
+
+    append_to_file 'app/assets/config/manifest.js', "\n//= link application.js\n" if File.exist?('app/assets/config/manifest.js')
+    gsub_file 'app/assets/config/manifest.js', '//= link_tree ../builds', ''
+    remove_dir 'app/assets/builds'
+  end
 end
